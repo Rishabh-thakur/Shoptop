c84c85bb6ded4f58929ab30787dd777690c3384c
Rishabh Singh <55924975+Rishabh-thakur@users.noreply.github.com>
2022-05-26 13:34:55 +0530

    Commiting shoptop

---
+++ src/components/checkoutForm/checkout/Checkout.jsx
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from "react";
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+  CssBaseline,
+} from "@material-ui/core";
+import useStyles from "./style";
+import { Link, useNavigate } from "react-router-dom";
+import AddressForm from "../AddressForm";
+import PaymentForm from "../PaymentForm";
+import { commerce } from "../../../lib/commerce";
+
+const steps = ["Shipping Address", "Payment details"];
+
+const Checkout = ({ cart, order, onCaptureCheckout, error ,onEmptyCart}) => {
+  const [activeStep, setActiveStep] = useState(0);
+  const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({});
+  const [isFinished, setIsFinished] = useState(false);
+
+  const classes = useStyles();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const generateToken = async () => {
+      try {
+        const Token = await commerce.checkout.generateToken(cart.id, {
+          type: "cart",
+        });
+        setCheckoutToken(Token);
+      } catch (error) {
+        navigate("/");
+      }
+    };
+    generateToken();
+  }, [cart]);
+
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  const next = (data) => {
+    setShippingData(data);
+    nextStep();
+  };
+
+  const timeout = () => {
+    setTimeout(() => {
+      setIsFinished(true);
+      onEmptyCart();
+    }, 3000);
+  };
+
+  let Confirmation = () =>
+    order.customer ? (
+      <>
+        <div>
+          <Typography variant="h5">
+            Thank you for your purchase , {order.customer.firstname}{" "}
+            {order.customer.lastname}
+          </Typography>
+          <Divider className={classes.divider} />
+          <Typography variant="subtitle2">
+            Order ref : {order.customer_reference}
+          </Typography>
+        </div>
+        <br />
+        <Button component={Link} to="/" variant="outlined" type="button">
+          Back to Home
+        </Button>
+      </>
+    ) : isFinished ? (
+      <>
+        <div>
+          <Typography variant="h5">Thank you for your purchase</Typography>
+          <Divider className={classes.divider} />
+        </div>
+        <br />
+        <Button component={Link} to="/" variant="outlined" type="button">
+          Back to Home
+        </Button>
+      </>
+    ) : (
+      <div className={classes.spinner}>
+        <CircularProgress />
+      </div>
+    );
+
+  if (error) {
+    <>
+      <Typography variant="h5">Error : {error}</Typography>
+      <br />
+      <Button component={Link} to="/" variant="outlined" type="button">
+        Back To Home
+      </Button>
+    </>;
+  }
+
+  const Form = () =>
+    activeStep === 0 ? (
+      <AddressForm checkoutToken={checkoutToken} next={next} />
+    ) : (
+      <PaymentForm
+        checkoutToken={checkoutToken}
+        shippingData={shippingData}
+        nextStep={nextStep}
+        backStep={backStep}
+        onCaptureCheckout={onCaptureCheckout}
+        timeout={timeout}
+      />
+    );
+
+  return (
+    <>
+      <CssBaseline />
+      <div className={classes.toolbar} />
+      <main style={{padding:"10px"}} className={classes.layout}>
+        <Paper className={classes.Paper}>
+          <Typography variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <Confirmation />
+          ) : (
+            checkoutToken && <Form />
+          )}
+        </Paper>
+      </main>
+    </>
+  );
+};
+
+export default Checkout;
---
+++ src/lib/commerce.js
@@ -0,0 +1,3 @@
+import Commerce from "@chec/commerce.js";
+
+export const commerce = new Commerce("pk_test_42787bececc7223462b9594a638ad685d92e605ba0b7e",true);
